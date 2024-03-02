81dc92907c97211c231547dbbdba024a3fd1dac5
Tom Bruno <tom.bruno@smartqasa.com>
2024-03-02 01:39:53 +0000

    v2.5

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ customElements.define('smartqasa-light-card2', SmartQasaLightCard2);
 window.customCards = window.customCards || [];
 window.customCards.push({
   type: 'smartqasa-light-card2',
-  name: 'SmartQasa Light Card - v2.4',
+  name: 'SmartQasa Light Card - v2.6',
   preview: true,
   description: 'A SmartQasa card for controlling a light entity.',
 });
