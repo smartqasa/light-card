56ce64562841bf30fd61f886b37f3c3a695ce1c5
Tom Bruno <tom.bruno@smartqasa.com>
2024-03-02 12:40:24 +0000

    v2.6

--- src/card.js
+++ src/card.js
@@ -1,6 +1,6 @@
 import { LitElement, html, css } from 'lit';
 
-export class SmartQasaLightCard2 extends LitElement {
+class SmartQasaLightCard2 extends LitElement {
 
   static get properties() {
     return {
@@ -119,3 +119,15 @@ export class SmartQasaLightCard2 extends LitElement {
     this.dispatchEvent(event);
   }
 }
+customElements.define(
+  'smartqasa-light-card2',
+  SmartQasaLightCard2
+);
+
+window.customCards = window.customCards || [];
+window.customCards.push({
+  type: 'smartqasa-light-card2',
+  name: 'SmartQasa Light Card - v2.7',
+  preview: true,
+  description: 'A SmartQasa card for controlling a light entity.',
+});
