a67fd1eb4c55d94c209f6e1a5198e6472d63f780
Tom Bruno <tom.bruno@smartqasa.com>
2024-03-04 01:55:10 +0000

    v2.9

--- dist/card.js
+++ dist/card.js
@@ -588,60 +588,117 @@ var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 parcelHelpers.export(exports, "SmartQasaLightCard2", ()=>SmartQasaLightCard2);
 var _lit = require("lit");
-var _styles = require("./styles");
-var _stylesDefault = parcelHelpers.interopDefault(_styles);
 class SmartQasaLightCard2 extends (0, _lit.LitElement) {
+    _hass;
     static get properties() {
         return {
-            hass: {
-                type: Object
+            _entity: {
+                state: true
             },
-            _config: {
-                type: Object
+            _stateObj: {
+                state: true
+            },
+            _icon: {
+                state: true
+            },
+            _name: {
+                state: true
+            },
+            _state: {
+                state: true
             }
         };
     }
-    static get styles() {
-        return 0, _stylesDefault.default;
-    }
-    constructor(){
-        super();
-        this.hass = {};
-        this._config = {};
-    }
     setConfig(config) {
-        if (!config.entity) throw new Error("You need to define an entity.");
-        this._config = config;
+        this._entity = config.entity;
+        this._icon = config.icon || null;
+        this._name = config.name || null;
+    }
+    set hass(hass) {
+        this._hass = hass;
+        this._stateObj = hass.states[this._entity];
+        if (this._stateObj) {
+            this._icon = this._stateObj.attributes.icon || "hass:alert-rhombus";
+            this._name = this._name || this._stateObj.attributes.friendly_name || "Unknown";
+            this._state = this._stateObj.state || "unknown";
+        }
+    }
+    static get styles() {
+        return (0, _lit.css)`
+      .container {
+        display: grid;
+        height: var(--sq-card-height, 4.0rem);
+        padding: var(--sq-card-padding, 0 1.0rem);
+        border: var(--sq-card-border, 'none');
+        border-radius: var(--sq-card-border-radius, 1.0rem);
+        box-shadow: var(--sq-card-box-shadow, 0 2px 4px 0 rgba(0, 0, 0, 0.2));
+        grid-template-areas: "i n" "i s";
+        grid-template-columns: auto 1fr;
+        grid-column-gap: var(--sq-card-gap-column, 0.7rem);
+        grid-row-gap: var(--sq-card-gap-row, 0.3rem);
+        background-color: var(--sq-card-background-color, rgba(192, 192, 192, 0.5));
+        cursor: pointer;
+      }
+      .icon {
+        grid-area: i;
+        display: flex;
+        height: var(--sq-icon-size, 25px);
+        width: var(--sq-icon-size, 25px);
+        justify-content: center;
+        align-self: center;
+        padding: var(--sq-icon-padding, 14px);
+        border-radius: 50%;
+        transition: var(--sq-icon-transition, none);
+      }
+      .name {
+        grid-area: n;
+        place-self: end start;
+        max-height: 3.5rem;
+        max-width: 100%;
+        text-align: left;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: normal;
+        font-weight: var(--sq-primary-font-weight, 400);
+        font-size: var(--sq-primary-font-size, 16px);
+        color: rgb(var(--sq-primary-font-rgb), 128, 128, 128);
+      }
+      .state {
+        grid-area: s;
+        align-self: start;
+        text-align: left;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        font-weight: var(--sq-secondary-font-weight, 300);
+        font-size: var(--sq-secondary-font-size, 14px);
+        color: rgb(var(--sq-secondary-font-rgb, 0, 0, 0));
+      }
+    `;
     }
     render() {
-        if (!this.hass || !this._config) return (0, _lit.html)``;
-        const stateObj = this.hass.states[this._config.entity];
-        const name = this._config.name || stateObj?.attributes.friendly_name || "Unknown";
-        const state = stateObj?.state || "unknown";
-        let icon, iconColor, stateFmtd;
-        if (stateObj) {
-            icon = stateObj.attributes.icon || "hass:alert-rhombus";
-            iconColor = state == "on" ? "var(--sq-light-on-rgb)" : "var(--sq-inactive-rgb)";
-            stateFmtd = this.hass.formatEntityState(stateObj) + (state == "on" && stateObj.attributes.brightness ? " - " + this.hass.formatEntityAttributeValue(stateObj, "brightness") : "");
+        if (!this._hass) return (0, _lit.html)``;
+        let iconColor, stateFmtd;
+        if (this._stateObj) {
+            iconColor = this._state == "on" ? "var(--sq-light-on-rgb)" : "var(--sq-inactive-rgb)";
+            stateFmtd = this._hass.formatEntityState(this._stateObj) + (this._state == "on" && this._stateObj.attributes.brightness ? " - " + this._hass.formatEntityAttributeValue(this._stateObj, "brightness") : "");
         } else {
-            icon = "hass:alert-rhombus";
             iconColor = "var(--sq-unavailable-rgb)";
             stateFmtd = "Unknown";
         }
         return (0, _lit.html)`
       <div class="container" @click=${this._showMoreInfo}>
         <div class="icon" @click=${this._toggleEntity} style="color: rgb(${iconColor}); background-color: rgba(${iconColor}, var(--sq-icon-opacity));">
-          <ha-icon .icon=${icon}></ha-icon>
+          <ha-icon .icon=${this._icon}></ha-icon>
         </div>
-        <div class="name">${name}</div>
+        <div class="name">${this._name}</div>
         <div class="state">${stateFmtd}</div>
       </div>
     `;
     }
     _toggleEntity(e) {
         e.stopPropagation();
-        this.hass.callService("light", "toggle", {
-            entity_id: this._config.entity
+        this._hass.callService("light", "toggle", {
+            entity_id: this._entity
         });
     }
     _showMoreInfo(e) {
@@ -650,14 +707,14 @@ class SmartQasaLightCard2 extends (0, _lit.LitElement) {
             bubbles: true,
             composed: true,
             detail: {
-                entityId: this._config.entity
+                entityId: this._entity
             }
         });
         this.dispatchEvent(event);
     }
 }
 
-},{"lit":"4antt","@parcel/transformer-js/src/esmodule-helpers.js":"9FYyo","./styles":"7add8"}],"4antt":[function(require,module,exports) {
+},{"lit":"4antt","@parcel/transformer-js/src/esmodule-helpers.js":"9FYyo"}],"4antt":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 var _reactiveElement = require("@lit/reactive-element");
@@ -1343,61 +1400,6 @@ parcelHelpers.defineInteropFlag(exports);
 parcelHelpers.export(exports, "isServer", ()=>o);
 const o = !1;
 
-},{"@parcel/transformer-js/src/esmodule-helpers.js":"9FYyo"}],"7add8":[function(require,module,exports) {
-var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
-parcelHelpers.defineInteropFlag(exports);
-var _lit = require("lit");
-exports.default = (0, _lit.css)`
-    .container {
-        display: grid;
-        height: var(--sq-card-height, 4.0rem);
-        padding: var(--sq-card-padding, 0 1.0rem);
-        border: var(--sq-card-border, 'none');
-        border-radius: var(--sq-card-border-radius, 1.0rem);
-        box-shadow: var(--sq-card-box-shadow, 0 2px 4px 0 rgba(0, 0, 0, 0.2));
-        grid-template-areas: "i n" "i s";
-        grid-template-columns: auto 1fr;
-        grid-column-gap: var(--sq-card-gap-column, 0.7rem);
-        grid-row-gap: var(--sq-card-gap-row, 0.3rem);
-        background-color: var(--sq-card-background-color, rgba(192, 192, 192, 0.5));
-        cursor: pointer;
-    }
-    .icon {
-        grid-area: i;
-        display: flex;
-        height: var(--sq-icon-size, 25px);
-        width: var(--sq-icon-size, 25px);
-        justify-content: center;
-        align-self: center;
-        padding: var(--sq-icon-padding, 14px);
-        border-radius: 50%;
-        transition: var(--sq-icon-transition, none);
-    }
-    .name {
-        grid-area: n;
-        place-self: end start;
-        max-height: 3.5rem;
-        max-width: 100%;
-        text-align: left;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: normal;
-        font-weight: var(--sq-primary-font-weight, 400);
-        font-size: var(--sq-primary-font-size, 16px);
-        color: rgb(var(--sq-primary-font-rgb), 128, 128, 128);
-    }
-    .state {
-        grid-area: s;
-        align-self: start;
-        text-align: left;
-        text-overflow: ellipsis;
-        overflow: hidden;
-        font-weight: var(--sq-secondary-font-weight, 300);
-        font-size: var(--sq-secondary-font-size, 14px);
-        color: rgb(var(--sq-secondary-font-rgb, 0, 0, 0));
-    }
-`;
-
-},{"lit":"4antt","@parcel/transformer-js/src/esmodule-helpers.js":"9FYyo"}]},["jM5Hw","gbMuj"], "gbMuj", "parcelRequire94c2")
+},{"@parcel/transformer-js/src/esmodule-helpers.js":"9FYyo"}]},["jM5Hw","gbMuj"], "gbMuj", "parcelRequire94c2")
 
 //# sourceMappingURL=card.js.map
--- src/card.js
+++ src/card.js
@@ -1,48 +1,101 @@
-import { LitElement, html } from 'lit';
-import styles from './styles';
+import { LitElement, html, css } from 'lit';
 
 export class SmartQasaLightCard2 extends LitElement {
 
+  _hass;
+
   static get properties() {
     return {
-      hass: { type: Object },
-      _config: { type: Object },
+      _entity: { state: true },
+      _stateObj: { state: true },
+      _icon: { state: true },
+      _name: { state: true },
+      _state: { state: true }
     };
   }
 
-  static get styles() {
-    return styles;
-  }
-
-  constructor() {
-    super();
-    this.hass = {};
-    this._config = {};
-  }
-
   setConfig(config) {
-    if (!config.entity) {
-      throw new Error('You need to define an entity.');
-    }
-    this._config = config;
+      this._entity = config.entity;
+      this._icon = config.icon || null;
+      this._name = config.name || null;
+  }
+
+  set hass(hass) {
+      this._hass = hass;
+      this._stateObj = hass.states[this._entity];
+      if (this._stateObj) {
+        this._icon = this._stateObj.attributes.icon || 'hass:alert-rhombus';
+        this._name = this._name || this._stateObj.attributes.friendly_name || "Unknown";
+        this._state = this._stateObj.state || 'unknown';
+      }
+  }
+
+  static get styles() {
+    return css`
+      .container {
+        display: grid;
+        height: var(--sq-card-height, 4.0rem);
+        padding: var(--sq-card-padding, 0 1.0rem);
+        border: var(--sq-card-border, 'none');
+        border-radius: var(--sq-card-border-radius, 1.0rem);
+        box-shadow: var(--sq-card-box-shadow, 0 2px 4px 0 rgba(0, 0, 0, 0.2));
+        grid-template-areas: "i n" "i s";
+        grid-template-columns: auto 1fr;
+        grid-column-gap: var(--sq-card-gap-column, 0.7rem);
+        grid-row-gap: var(--sq-card-gap-row, 0.3rem);
+        background-color: var(--sq-card-background-color, rgba(192, 192, 192, 0.5));
+        cursor: pointer;
+      }
+      .icon {
+        grid-area: i;
+        display: flex;
+        height: var(--sq-icon-size, 25px);
+        width: var(--sq-icon-size, 25px);
+        justify-content: center;
+        align-self: center;
+        padding: var(--sq-icon-padding, 14px);
+        border-radius: 50%;
+        transition: var(--sq-icon-transition, none);
+      }
+      .name {
+        grid-area: n;
+        place-self: end start;
+        max-height: 3.5rem;
+        max-width: 100%;
+        text-align: left;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: normal;
+        font-weight: var(--sq-primary-font-weight, 400);
+        font-size: var(--sq-primary-font-size, 16px);
+        color: rgb(var(--sq-primary-font-rgb), 128, 128, 128);
+      }
+      .state {
+        grid-area: s;
+        align-self: start;
+        text-align: left;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        font-weight: var(--sq-secondary-font-weight, 300);
+        font-size: var(--sq-secondary-font-size, 14px);
+        color: rgb(var(--sq-secondary-font-rgb, 0, 0, 0));
+      }
+    `;
   }
 
   render() {
-    if (!this.hass || !this._config) {
+    if (!this._hass) {
       return html``;
     }
 
-    const stateObj = this.hass.states[this._config.entity];
-    const name = this._config.name || stateObj?.attributes.friendly_name || 'Unknown';
-    const state = stateObj?.state || 'unknown';
-    let icon, iconColor, stateFmtd;
+    let iconColor, stateFmtd;
 
-    if (stateObj) {
-      icon = stateObj.attributes.icon || 'hass:alert-rhombus';
-      iconColor = state == 'on' ? 'var(--sq-light-on-rgb)' : 'var(--sq-inactive-rgb)';
-      stateFmtd = this.hass.formatEntityState(stateObj) + (state == 'on' && stateObj.attributes.brightness ? ' - ' + this.hass.formatEntityAttributeValue(stateObj, 'brightness') : '');
+    if (this._stateObj) {
+      iconColor = this._state == 'on' ? 'var(--sq-light-on-rgb)' : 'var(--sq-inactive-rgb)';
+      stateFmtd = this._hass.formatEntityState(this._stateObj) +
+        (this._state == 'on' && this._stateObj.attributes.brightness ? ' - ' +
+        this._hass.formatEntityAttributeValue(this._stateObj, 'brightness') : '');
     } else {
-      icon = 'hass:alert-rhombus';
       iconColor = 'var(--sq-unavailable-rgb)';
       stateFmtd = 'Unknown';
     }
@@ -50,9 +103,9 @@ export class SmartQasaLightCard2 extends LitElement {
     return html`
       <div class="container" @click=${this._showMoreInfo}>
         <div class="icon" @click=${this._toggleEntity} style="color: rgb(${iconColor}); background-color: rgba(${iconColor}, var(--sq-icon-opacity));">
-          <ha-icon .icon=${icon}></ha-icon>
+          <ha-icon .icon=${this._icon}></ha-icon>
         </div>
-        <div class="name">${name}</div>
+        <div class="name">${this._name}</div>
         <div class="state">${stateFmtd}</div>
       </div>
     `;
@@ -60,14 +113,14 @@ export class SmartQasaLightCard2 extends LitElement {
 
   _toggleEntity(e) {
     e.stopPropagation();
-    this.hass.callService('light', 'toggle', {
-      entity_id: this._config.entity
+    this._hass.callService('light', 'toggle', {
+      entity_id: this._entity
     });
   }
 
   _showMoreInfo(e) {
     e.stopPropagation();
-    const event = new CustomEvent('hass-more-info', { bubbles: true, composed: true, detail: { entityId: this._config.entity } });
+    const event = new CustomEvent('hass-more-info', { bubbles: true, composed: true, detail: { entityId: this._entity } });
     this.dispatchEvent(event);
   }
 }
