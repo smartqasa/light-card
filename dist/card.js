732c6ab7ef2cf52236be336d8ba2822a0c3580e7
Tom Bruno <tom.bruno@smartqasa.com>
2024-03-04 19:08:45 +0000

    v2.10

--- dist/card.js
+++ dist/card.js
@@ -717,6 +717,14 @@ class SmartQasaLightCard2 extends (0, _lit.LitElement) {
         this.dispatchEvent(event);
     }
 }
+customElements.define("smartqasa-light-card2", SmartQasaLightCard2);
+window.customCards = window.customCards || [];
+window.customCards.push({
+    type: "smartqasa-light-card2",
+    name: "SmartQasa Light Card2",
+    preview: true,
+    description: "A SmartQasa card for controlling a light entity."
+});
 
 },{"lit":"4antt","@parcel/transformer-js/src/esmodule-helpers.js":"9FYyo"}],"4antt":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
